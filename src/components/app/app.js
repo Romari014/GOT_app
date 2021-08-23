5aff87934e0d4829d5d902ed312594b3ea44eb54
Roman <roma00062@gmail.com>
2021-08-23 18:26:28 +0200

    Refactored code by pattern

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,23 +1,30 @@
 import React, { Component } from 'react';
-import {Col, Row, Container} from 'reactstrap';
+import { Col, Row, Container } from 'reactstrap';
 import Header from '../header';
 import RandomChar from '../randomChar';
 import ErrorMessage from '../errorMessage/errorMessage';
 import CharacterPage from '../characterPage/characterPage';
+import ItemList from '../itemList';
+import CharDetails from '../charDetails';
+import gotService from '../../services/gotService';
 import './app.css';
 
 
 
+
 export default class App extends Component {
+
+    gotService = new gotService();
+
     state = {
         showRandomChar: true,
         error: false,
     }
-    
+
     componentDidCatch() {
         console.log('error');
         this.setState({
-            error:true
+            error: true
         })
     }
 
@@ -30,32 +37,55 @@ export default class App extends Component {
     }
 
     render() {
-        
+
         if (this.state.error) {
-            return <ErrorMessage/>
+            return <ErrorMessage />
         }
 
-        const char = this.state.showRandomChar ?  <RandomChar/> : null;
+        const char = this.state.showRandomChar ? <RandomChar /> : null;
 
         return (
-            <> 
+            <>
                 <Container>
                     <Header />
                 </Container>
                 <Container>
                     <Row>
-                        <Col lg={{size: 5, offset: 0}}>
-                                {char}
-                                <button  
-                                    onClick={this.toggleRandomChar}
-                                    color="primary"
-                                    className="toggleBtn"
-                                >
-                                         Toggle random character
-                                </button>
+                        <Col lg={{ size: 5, offset: 0 }}>
+                            {char}
+                            <button
+                                onClick={this.toggleRandomChar}
+                                color="primary"
+                                className="toggleBtn"
+                            >
+                                Toggle random character
+                            </button>
                         </Col>
                     </Row>
-                        <CharacterPage/>
+                    <CharacterPage />
+                    <Row>
+                        <Col md='6'>
+                            <ItemList 
+                                onItemSelected={this.onItemSelected} 
+                                getData={this.gotService.getAllBooks}
+                                renderItem={(item) => item.name} />
+                        </Col>
+                        <Col md='6'>
+                            <CharDetails charId={this.state.selectedChar} />
+                        </Col>
+
+                        
+                            <Col md='6'>
+                                <ItemList 
+                                    onItemSelected={this.onItemSelected} 
+                                    getData={this.gotService.getAllHouses}
+                                    renderItem={(item) => item.name } />
+                            </Col>
+                            <Col md='6'>
+                                <CharDetails charId={this.state.selectedChar} />
+                            </Col>
+                        
+                    </Row>
                 </Container>
             </>
         );
--- src/components/itemList/itemList.js
+++ src/components/itemList/itemList.js
@@ -1,33 +1,35 @@
 import React, {Component} from 'react';
-import gotService from '../../services/gotService';
 import Spinner from '../spinner/spinner';
 import './itemList.css';
 
 export default class ItemList extends Component {
 
-    gotService = new gotService();
-
     state = {
-        charList: null
+        itemList: null
     }
 
-     componentDidMount() {
-         this.gotService.getAllCharacters()
-            .then((charList) => {
+     componentDidMount() {//hooks
+
+        const {getData} = this.props;
+
+         getData()
+            .then((itemList) => {
                 this.setState({
-                    charList
+                    itemList
                 })
             })
      } 
      
      renderItem(arr) {
          return arr.map((item, i) => {
+            const {id} = item;
+            const label = this.props.renderItem(item)
             return (
                 <li 
-                    key={i}
+                    key={id}
                     className="list-group-item"
-                    onClick={ () => this.props.onCharSelected(41 + i)}>
-                    {item.name}
+                    onClick={ () => this.props.onItemSelected(id)}>
+                    {label}
                 </li>
             )
          });
@@ -35,13 +37,13 @@ export default class ItemList extends Component {
 
     render() {
 
-        const {charList} = this.state;
+        const {itemList} = this.state;
 
-        if (!charList) {
+        if (!itemList) {
            return <Spinner/>
         }
 
-        const items = this.renderItem(charList);
+        const items = this.renderItem(itemList);
 
         return (
             <ul className="item-list list-group">

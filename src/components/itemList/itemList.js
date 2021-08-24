da7324aa69bc307991f76719694f1ae6bf134008
Roman <roma00062@gmail.com>
2021-08-24 18:51:07 +0200

    Created  HOS component

--- src/components/itemList/itemList.js
+++ src/components/itemList/itemList.js
@@ -1,23 +1,10 @@
 import React, { Component } from 'react';
 import Spinner from '../spinner/spinner';
+import gotServise from '../../services/gotService';
 import './itemList.css';
 
-export default class ItemList extends Component {
+class ItemList extends Component {
 
-    state = {
-        itemList: null
-    }
-
-    componentDidMount() {
-        const { getData } = this.props;
-
-        getData()
-            .then((itemList) => {
-                this.setState({
-                    itemList
-                })
-            })
-    }
 
     renderItems(arr) {
         return arr.map((item) => {
@@ -37,13 +24,8 @@ export default class ItemList extends Component {
     }
 
     render() {
-        const { itemList } = this.state;
-
-        if (!itemList) {
-            return <Spinner />
-        }
-
-        const items = this.renderItems(itemList);
+        const {data} = this.props;
+        const items = this.renderItems(data);
 
 
         return (
@@ -56,5 +38,37 @@ export default class ItemList extends Component {
 
 
 ItemList.defaultProps = {
-    onItemSelected: () => {}
+    onItemSelected: () => { }
 }
+
+
+
+const withData = (View, getData) => { //HOS function
+    return class extends Component {
+
+        state = {
+            data: null
+        }
+
+        componentDidMount() {//life cycle hook
+
+            getData()
+                .then((data) => {
+                    this.setState({
+                        data
+                    })
+                })
+        }
+
+        render() {
+            const { data } = this.state;
+
+            if (!data) {
+                return <Spinner />
+            }
+            return <View {...this.props}  data={data}/>
+        }
+    }
+}
+const {getAllCharacters} = new gotServise();
+export default withData(ItemList, getAllCharacters);
